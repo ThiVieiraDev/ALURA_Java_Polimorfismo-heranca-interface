f876ca4b27ac503286a66f4de3219c57a37d5995
Thi Vieira <thiiagoviieira@gmail.com>
2021-02-15 15:35:04 -0300

    Criação da classe Cliente implementando a interface Autenticavel

---
+++ src/Cliente.java
@@ -0,0 +1,12 @@
+public class Cliente implements Autenticavel {
+
+    @Override
+    public void setSenha(int senha) {
+
+    }
+
+    @Override
+    public boolean autentica(int senha) {
+        return false;
+    }
+}
