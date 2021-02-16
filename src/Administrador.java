16c70c8cfdb1d981dfa0cf2c76097519bd360778
Thi Vieira <thiiagoviieira@gmail.com>
2021-02-16 00:16:52 -0300

    Ajustando classe Administrador com nova classe AutenticacaoUtil

--- src/Administrador.java
+++ src/Administrador.java
@@ -1,22 +1,23 @@
 public class Administrador extends Funcionario implements Autenticavel{
-    private int senha;
 
-    public double getBonificacao(){
-        return 50;
+    private AutenticacaoUtil autenticador;
+
+    public Administrador(){
+        this.autenticador = new AutenticacaoUtil();
     }
 
     @Override
     public void setSenha(int senha){
-        this.senha = senha;
+        this.autenticador.setSenha(senha);
     }
 
     @Override
     public boolean autentica(int senha){
+        return this.autenticador.autentica(senha);
+    }
 
-        if(this.senha == senha){
-            return true;
-        } else {
-            return false;
-        }
+    @Override
+    public double getBonificacao() {
+        return 50;
     }
 }
--- src/Gerente.java
+++ src/Gerente.java
@@ -1,26 +1,25 @@
 //Gerente é um Funcionário, e também herda da classe Funcionário. Assina o contrato Autenticavel
 public class Gerente extends Funcionario implements Autenticavel{
 
-    private int senha;
+    private AutenticacaoUtil autenticador;
 
+    public Gerente(){
+        this.autenticador = new AutenticacaoUtil();
+    }
+
+    @Override
+    public void setSenha(int senha){
+        this.autenticador.setSenha(senha);
+    }
+
+    @Override
+    public boolean autentica(int senha){
+        return this.autenticador.autentica(senha);
+    }
+
+    @Override
     public double getBonificacao() {
-        System.out.println("Chamando o método debonificação do GERENTE:");
+        System.out.println("Chamando o método de bonificacao do GERENTE");
         return super.getSalario();
-    }
-
-    @Override
-    public void setSenha(int senha) {
-        this.senha = senha;
-
-    }
-
-    @Override
-    public boolean autentica(int senha) {
-        if (this.senha == senha){
-            return true;
-        }
-        else {
-            return false;
-        }
     }
 }
